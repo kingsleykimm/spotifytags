e5a5e3ab57f8f2f33b561d8655b28047f7f5b962
Kingsley Kim <kingsleykimm@gmail.com>
2023-12-18 18:18:22 -0500

    Sample login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,22 @@
 import styles from "../page.module.css"
 // server-side component
-
+import { useSession, signIn, signOut } from "next-auth/react"
 export default function LoginPage() {
 
-
+    const {data : session } = useSession()
+    if (session) {
+        return (
+            <>
+            Signed in as {session.user!.email} <br />
+            <button onClick={() => signOut()}>Sign out</button>
+            </>
+        )
+    }
+    return (
+        <>
+        Not signed in <br />
+        <button onClick={() => signIn()}>Sign In</button>
+        </>
+    )
 
 }
